0fedcbc645c2a751d277ee98ee47d05635d1d8a3
marceggl <62616163+Marceggl@users.noreply.github.com>
2023-08-29 13:20:58 -0300

    Hello World

---
+++ C++/hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>                 // operações de entrada e saida
+
+int main(){
+    std::cout << "Hello World!";    // Irá enviar uma saida para o sistema
+    return 0;                       // Fim do código
+}
